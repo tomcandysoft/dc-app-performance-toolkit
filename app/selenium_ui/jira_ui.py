2acc7fd8c87b4874952dc669fb074e3b7ccf5142
Tom Tran <tom@candylio.com>
2021-01-14 10:07:29 +1100

    Add app-specific action

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -61,7 +61,7 @@ Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
 Refer to `app/selenium_ui/jira/modules.py` for examples.
 """
 # def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+extension_ui.app_specific_action(jira_webdriver, jira_datasets)
 
 
 # this action should be the last one
